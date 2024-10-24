20f21326a709ea4b9df017da1b847d731f087326
cristianfrpt <baixinho_cris@hotmail.com>
2024-10-23 21:09:38 -0300

    Configured and applied migrations for ASP.NET Core Identity.

--- HelpPlatform/src/HelpPlatform.Infrastructure/Identity/ApplicationDbContext.cs
+++ HelpPlatform/src/HelpPlatform.Infrastructure/Identity/ApplicationDbContext.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace HelpPlatform.Infrastructure.Identity;
 public class ApplicationDbContext : IdentityDbContext<ApplicationUser>  
@@ -17,6 +14,13 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
             base.OnModelCreating(builder);
             builder.Entity<ApplicationUser>().Property(u => u.Initials).HasMaxLength(5);
 
-            builder.HasDefaultSchema("identity");
+            // Remove schema configuration
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers");
+            builder.Entity<IdentityRole>().ToTable("AspNetRoles");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("AspNetUserClaims");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("AspNetUserLogins");
+            builder.Entity<IdentityUserRole<string>>().ToTable("AspNetUserRoles");
+            builder.Entity<IdentityUserToken<string>>().ToTable("AspNetUserTokens");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("AspNetRoleClaims");
     }
 }
